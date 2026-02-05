1137a5bb874f6608212a3c6da9734441e927bc43
x24omkara <x24omkara@iima.ac.in>
2026-02-05 17:02:22 +0530

    Update Clustering.py

--- streamlit-dashboard/pages/Clustering.py
+++ streamlit-dashboard/pages/Clustering.py
@@ -1,8 +1,12 @@
 # import streamlit as st
 # import plotly.express as px
 # from utils import load_data, preprocess, build_features, do_clustering
+import pandas as pd
+import numpy as np
+import streamlit as st
 
 from utils import load_data, preprocess, build_features, do_clustering
+
 
 df = preprocess(load_data())
 features = build_features(df)
@@ -22,4 +26,5 @@ features_clust, best = do_clustering(features)
 #                  color="cluster", hover_data=["Group Company"],
 #                  title="Competitor Clustering")
 # st.plotly_chart(fig, use_container_width=True)
+
 
